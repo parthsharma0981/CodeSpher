16a4deed13589f651f5018095139b59938c60e1f
parthsharma0981 <parthsharma0981@gmail.com>
2026-08-24 13:39:37 +0530

    Remove non-project files and update gitignore

--- server/src/sockets/notificationSocket.js
+++
@@ -1,7 +0,0 @@
-export default (io, socket) => {
-  // Emitting a notification from backend to specific user
-  socket.on('send_notification', (data) => {
-    // Expected data: { userId, notification }
-    io.to(data.userId).emit('new_notification', data.notification);
-  });
-};
